7e9e008fc747b8a8b7e69cf4c9199031b7fb3de2
Jitendra Pratap <jitendrapratap432@gmail.com>
2023-09-13 21:13:09 +0530

    Frontend code

---
+++ ani-home/src/components/Card/AniCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Card, ListGroup, Button } from "react-bootstrap";
+import Col from 'react-bootstrap/Col';
+
+import { Link } from "react-router-dom";
+
+const AniCard = ({ data }) => {
+  return (
+    <>
+      {data.map((ele) => {
+        return (
+          <Card style={{ width: "18rem", marginTop: "0.5rem" }}>
+            <Card.Img src={ele.photo} />
+            <Card.Body>
+              <Card.Title>{ele.name.toUpperCase()}</Card.Title>
+              <Card.Text>
+                {ele.description[0].toUpperCase() +
+                  ele.description.slice(1).toLowerCase()}
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>
+                City:{" "}
+                {ele.city[0].toUpperCase() + ele.city.slice(1).toLowerCase()}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Posted By :{" "}
+                {ele.username[0].toUpperCase() +
+                  ele.username.slice(1).toLowerCase()}
+              </ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">
+                <Link to={"/addorder/" + ele.username + "/" + ele.id}>
+                  <Button variant="primary">Adopt</Button>
+                </Link>
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </>
+  );
+};
+
+export default AniCard;
